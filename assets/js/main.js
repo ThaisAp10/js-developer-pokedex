aca1846f7173b413ce1b540ff5e0022ef6ca5c42
ThaisAp10 <thaisapare.10@gmail.com>
2023-05-27 16:57:36 -0300

    Atualizado o projeto com as informações de ao clicar no pokemon carregar alguns detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" onClick="pokemonClick(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -21,6 +21,62 @@ function convertPokemonToLi(pokemon) {
             </div>
         </li>
     `
+}
+
+// evento click no pokemon da lista
+
+const pokemonClick = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const resp = await fetch(url)
+    const pokemon = await resp.json()
+    displayPokemon(pokemon)
+}
+
+// abre a tela com as informações do pokemon
+
+const displayPokemon = (pokemon) => {
+    const imagem = pokemon.sprites.other.dream_world.front_default
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    const generateHtml = `
+    <div class="display">
+    <div id="details">
+        <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.id}</span>
+        <span class="name">#${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+        </div>
+    <img id="pokemon-img" src="${imagem}"
+        alt="${pokemon.name}">
+    <div id="data">
+        <h4>Infos</h4>
+    <div id="small-info">
+    </div>
+    <div class="small-info-data">
+        <p class="${type}">Height:${(pokemon.height/10).toFixed(2)} m</p> 
+        <p class="${type}">Weight:${(pokemon.weight/10)} kg</p>
+    </div>
+    </li>
+    <button id="closeBtn" onClick="closeDisplay()">X</button>
+    </div> 
+    </div>
+    </div>                  
+    `
+
+    pokemonList.innerHTML = generateHtml + pokemonList.innerHTML
+}
+
+//Fechar a tela
+const closeDisplay = () => {
+    const display = document.querySelector('.display')
+    display.parentElement.removeChild(display)
 }
 
 function loadPokemonItens(offset, limit) {
